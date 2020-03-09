00782540c2464e118bc1c67fb8bca967d519598e
tinaba96 <tinaba178.96@gmail.com>
2020-03-09 13:55:14 +0900

    update

--- acode/abc158.py
+++ acode/abc158.py
@@ -83,3 +83,50 @@ if A%2 != 0:
     S.reverse()
 print(''.join(S))
 '''
+#d
+is=str(input())
+
+N=int(input())
+cnt=0
+f=''
+b=''
+for i in range(N):
+  inp=str(input())
+  if len(inp)==1:
+    cnt+=1
+  else:
+    F,c=int(inp[2]),inp[4]
+    if F==1:
+      if cnt%2==0:
+        f=c+f
+      else:
+        b+=c
+    else:
+      if cnt%2==0:
+        b+=c
+      else:
+        f=c+f
+if cnt%2==0:
+  print(f+s+b)
+else:
+  ans=f+s+b
+  L=len(ans)
+  if L==1:
+    print(ans)
+  else:
+    if L%2==1:
+      f=ans[0:L//2]
+      s=ans[L//2]
+      b=ans[L//2+1:L]
+      f_=''.join(list(reversed(b)))
+      b_=''.join(list(reversed(f)))
+      print(f_+s+b_)
+    else:
+      f=ans[0:L//2]
+      b=ans[L//2:L]
+      f_=''.join(list(reversed(b)))
+      b_=''.join(list(reversed(f)))
+      print(f_+b_)
+
+
+
