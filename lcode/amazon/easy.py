ec453365e04da6c77b5f5bf22a9fe6c76e0bb538
tinaba96 <tinaba178.96@gmail.com>
2020-04-10 11:10:58 +0900

    update

--- lcode/amazon/easy.py
+++ lcode/amazon/easy.py
@@ -57,36 +57,97 @@ class Solution:
                 q.append((current_depth+1, root.right))
         return depth
 #ok
-
 #572
     def isSubtree(self, s:TreeNode, t:TreeNode) -> bool:
         flag = False
-        qs = []
-        qt = []
-        qs.append(s)
-        qt.append(t)
+        stack_s = []
+        stack_t = []
+        stack_s.append(s)
+        stack_t.append(t)
 
-        while len(qs) > 0:
-            current = qs.pop()
-            if current == qt[0] :
-                qt.pop()
+        while len(stack_s) > 0:
+            current_s = stack_s.pop()
+            if current_s == stack_t.pop() :
+                current_t = current_s
                 flag = True
-                qt.append(t.left)
-                qt.append(t.right)
+                if current_t.left != None:
+                    stack_t.append(current_t.left)
+                if current_t.right != None:
+                    stack_t.append(current_t.right)
+                if current_t.left == None and current_t.right == None:
+                    return True
             else:
                 flag = False
-                qt = []
-                qt.append(t)
-            qs.append(s.left)
-            qs.append(s.right)
-            if qs == [] and qt == []:
-                break
+                stack_t = []
+                stack_t.append(t)
+            if current_s.left != None:
+                stack_s.append(current_s.left)
+            if current_s.right != None:
+                stack_s.append(current_s.right)
 
-        if flag == True and t.right == None and t.left == None:
+        if flag == True:
             return True
         else:
             return False
 
+    #ans
+    def isSubtree(self, s:TreeNode, t:TreeNode) -> bool:
+        a = []
+        self.preorder(s, a)
+        b = []
+        self.preorder(t, b)
+        a = ''.join(a)
+        b = ''.join(b)
+    
+        if b in a:
+            return True
+        else:
+            return False
+
+    def preorder(self, node, l):
+        if not node:
+            l.append("#")
+            return
+
+        l.append(':'+str(node.val))
+        self.preorder(node.left, l)
+        self.preorder(node.right, l)
+
+    def isSubtree(self, s: TreeNode, t:TreeNode) -> bool:
+        return self.dfs(s, t)
+
+    def dfs(self, node1, node2):
+        if not model:
+            return False
+
+        if node1.val == node2.val:
+            if self.check(node1, node2):
+                return True
+        if self.dfs(node1.left, node2):
+            return True
+        if self.dfs(node1.right, node2):
+            return True
+
+        return False
+
+    def check(self, node1, node2):
+        if not node1 and not node2:
+            return True
+        if not node1 or not node2:
+            return False
+        if node1.val != node2.val:
+            return False
+
+        return self.check(node1.left, node2.left) and self.check(node1.right, node2.right)
+
+    import collections
+    def majorityElement(self, nums: list[int]) -> int:
+        c = collections.Counter(nums)
+        return c.most_common()[0][0]
+
+    def majorityElement(self, nums):
+        nums.sort()
+        return nums[len(nums)//2]
 
 
 if __name__ == "__main__":
