ef6e273270d59b0a67dddf02701d63c04cd494d9
tinaba96 <tinaba178.96@gmail.com>
2020-04-12 13:47:22 +0900

    upadte

--- lcode/amazon/easy.py
+++ lcode/amazon/easy.py
@@ -160,6 +160,7 @@ class Solution:
         return candidate
 
     #496
+    #O(m*n)
     def nextGreaterElement(self, nums1:List[int], nums2:List[int]) -> List[int]:
         ans = []
         for ele in nums1:
@@ -173,6 +174,22 @@ class Solution:
                 ans.append(-1)
 
         return ans
+
+    #O(m*n)
+    def nextGreaterElement(self, nums1:List[int], nums2:List[int]) -> List[int]:
+        d = {}
+        ans = [-1]*len(nums1)
+        for i, num in enumerate(nums1):
+            d[num] = i
+        stack = []
+        for num in nums:
+            while stack and stack[-1] < num:
+                top = stack.pop()
+                if top in d:
+                    ans[d[top]] = num
+                stck.append(num)
+            return ans
+
 
 
 if __name__ == "__main__":
--- lcode/amazon/medium.py
+++ lcode/amazon/medium.py
@@ -12,17 +12,57 @@ class Solution:
                     next(a, b+1)
                 if a+1 < len(grid):
                     next(a+1, b)
-
+                if a-1 >= 0:
+                    next(a-1, b)
+                if b-1 >= 0:
+                    next(a, b-1)
             return 1
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 1:
+                if grid[i][j] == '1':
                     cnt += next(i, j)
-                    if i+1 < len(grid):
-                        cnt += next(i+1, j)
-                    if j+1 < len(grid[0]):
-                        cnt += next(i, j+1)
 
         return cnt
+
+    #unionfind
+    from collections import defaultdict
+    def numIslands(self, grid):
+            """
+            :type grid: List[List[str]]
+            :rtype: int
+            """
+            if len(grid) == 0 or len(grid[0]) == 0:
+                    return 0
+            totalRow, totalCol = len(grid), len(grid[0])
+            parent = defaultdict()
+            self.count = 0
+            for i in range(totalRow):
+                    for j in range(totalCol):
+                            if grid[i][j] == "1":
+                                    parent[i*totalCol+j] = -1
+                                    self.count+=1
+
+            for i in range(totalRow):
+                    for j in range(totalCol):
+                            if grid[i][j] == "1":
+                                    r,c = i,j
+                                    for (dr,dc) in [(-1,0),(1,0),(0,-1),(0,1)]:
+                                            newRow, newCol = r + dr, c + dc
+                                            if 0<=newRow<totalRow and 0<=newCol<totalCol and grid[newRow][newCol] == "1":
+                                                    self.union(parent, r*totalCol+c, newRow*totalCol+newCol)
+            return self.count
+
+    def find(self, parent, i):
+            if parent[i] != -1:
+                    return self.find(parent, parent[i])
+            else:
+                    return i
+
+    def union(self, parent, a, b):
+            rootA = self.find(parent,a)
+            rootB = self.find(parent,b)
+            if rootA != rootB:
+                    parent[rootA] = rootB
+                    self.count -= 1
 
