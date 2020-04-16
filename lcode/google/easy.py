fa57e801af86b6ee2b3bb3ae64eaf6e883f8ff80
tinaba96 <tinaba178.96@gmail.com>
2020-04-17 08:43:40 +0900

    update

--- lcode/google/easy.py
+++ lcode/google/easy.py
@@ -3,11 +3,19 @@ class Solution:
     def getHint(self, secret: str, guess:str) -> str:
         bulls = 0
         cows = 0
+        li = []
         for i in range(len(guess)):
             if guess[i] == secret[i]:
                 cows += 1
-            if guess[i] in secret:
-                bulls += 1
-        return str(cows) + 'A' + (bulls-cows) + 'B'
+                li.append(i)
+        flag
+        for i in range(len(guess)):
+                for j in range(len(secret)):
+                    if guess[i] == secret[j]:
+                        if j not in li:
+                            bulls += 1
+                            li.append(j)
+        return str(cows) + 'A' + str(bulls) + 'B'
+
 
 
