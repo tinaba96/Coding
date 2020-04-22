9bc1ab2f42769950320673e9decba2c0cfe95e64
tinaba96 <tinaba178.96@gmail.com>
2020-04-22 12:59:54 +0900

    update

--- acode/abc163.py
+++ acode/abc163.py
@@ -16,7 +16,8 @@ if N < sum(A):
     print(-1)
 else:
     print(N-sum(A))
-'''
+
+
 #C
 import collections
 N = int(input())
@@ -46,7 +47,55 @@ def count(N, K):
 
 count(N, K)
 print(ans%(10**9+7))
+'''
+
+#Dans
+N, K = map(int, input().split())
+mod = 1000000007
+
+ans = 0
+
+for i in range(K, N + 2):
+    min_candidate = ((i - 1) * i * (1/2)) % mod
+    max_candidate = ((2 * N + 1 - i) * i * (1/2)) % mod #初項N-i、末項N+1数列がi個ある。
+    ans = ans + (max_candidate - min_candidate + 1) 
+print(int(ans % mod)) #//を使ってない分intで整数に直している。
+
+#計算上少数が生じることはないので、//やint()は必要ないと思われる。
+#しかし、.0と、少数まで表示されてしまうため、上記は結局必要
+#理由は、割り算の計算(/)をすると、少数まで表示されるようになっているから。
+'''
+
+#another
+N, K = map(int, input().split())
+
+ans = 0
+MOD = 10**9 + 7
 
 
+def sum(a, b):
+    return (b - a + 1) * (a + b) // 2
 
 
+for i in range(K, N + 2):
+    min = sum(0, i - 1)
+    max = sum(N - (i - 1), N)
+    ans += max - min + 1
+
+print(ans % MOD)
+
+
+#another
+def my_code_D():
+    mod = 10**9+7
+    n,k = map(int, input().split())
+    res = 0
+    for i in range(k,n+1):
+        count = (n*i - i*(i-1) + 1)%mod
+        res = (res + count)%mod
+    res = (res+1)%mod # +1はk=N+1の文である。N+1の場合常に1通りであるため。
+    print(res)
+
+my_code_D()
+
+'''
