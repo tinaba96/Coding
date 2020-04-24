c283d0a88227f4211ec5257178086f3eced58939
tinaba96 <tinaba178.96@gmail.com>
2020-04-24 23:36:54 +0900

    update

--- lcode/google/easy.py
+++ lcode/google/easy.py
@@ -78,11 +78,40 @@ class Logger(object):
         else:
             list.append((timestamp, message))
             return True
-
-
-
 # Your Logger object will be instantiated and called as such:
 # obj = Logger()
 # param_1 = obj.shouldPrintMessage(timestamp,message)
+
+
+#ans
+from collections import deque
+
+class Logger(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self._msg_set = set()
+        self._msg_queue = deque()
+
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        """
+        while self._msg_queue:
+            msg, ts = self._msg_queue[0]
+            if timestamp - ts >= 10:
+                self._msg_queue.popleft()
+                self._msg_set.remove(msg)
+            else:
+                break
+
+        if message not in self._msg_set:
+            self._msg_set.add(message)
+            self._msg_queue.append((message, timestamp))
+            return True
+        else:
+            return False
 
 
