1ab1afe318011461f35a4eef213f974eb6a18ca0
tinaba96 <tinaba178.96@gmail.com>
2020-04-24 22:37:08 +0900

    update

--- lcode/google/easy.py
+++ lcode/google/easy.py
@@ -53,3 +53,36 @@ class Solution:
         
         return f"{bulls}A{cows}B"
 
+#359
+class Logger(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.time = 0
+        self.message = 0
+
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        :type timestamp: int
+        :type message: str
+        :rtype: bool
+        """
+        list = {}
+        if message in list:
+            return False
+        else:
+            list.append((timestamp, message))
+            return True
+
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
+
+
