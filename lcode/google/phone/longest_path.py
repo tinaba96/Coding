56811c58d77461ea8a2555759f8bdd5fe7f6eac5
tinaba96 <tinaba178.96@gmail.com>
2020-04-28 19:09:53 +0900

    update

--- lcode/google/phone/longest_path.py
+++ lcode/google/phone/longest_path.py
@@ -1,14 +1,15 @@
 
 
+m, n = list(map(int, input().split()))
 
-mat = {}
-m, n = list(map(int, input().split())
+mat = [[-1]*n for i in range(m)]
 for j in range(m):
-mat[j+1] = list(map(int, input().split())
+    mat[j+1] = list(map(int, input().split()))
 longest = 0
+print(mat)
 
 def path_from(i, j, cnt):
-	if mat[i][j+1] = mat[i][j] + 1:
+	if mat[i][j+1] == mat[i][j] + 1:
 		max = max(path_from(i, j+1, cnt + 1), cnt)
 	if mat[i+1][j] == mat[i][j] + 1:
 		max = max(path_from(i+1, j, cnt +1), cnt)
@@ -18,11 +19,13 @@ def path_from(i, j, cnt):
 		max = max(path_from(i, j-1, cnt + 1), cnt)
 	return max
 
-def longest_path(m, n):   // 与えられる行列の大きさ
+def longest_path(m, n):   # 与えられる行列の大きさ
 	for i in range(m):
 		for j in range(n):
 			cnt = path_from(i, j, 1)
 			longest = max(longest, cnt)
 	return longest
+
+longest_path(m,n )
 
 
