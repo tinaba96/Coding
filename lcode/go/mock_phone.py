ec5ff861e9cd657fc9164692f1648872b6ae7c44
tinaba96 <tinaba178.96@gmail.com>
2020-05-02 18:09:14 +0900

    update

--- lcode/go/mock_phone.py
+++ lcode/go/mock_phone.py
@@ -12,10 +12,29 @@ class Solution(object):
             if n-1-a < 0:
                 digits.insert(0, 1)
                 return digits
-        digits[n-1-a] += 1  #input [9]のときの挙動チェック
+        digits[n-1-a] += 1  
         return digits
 
     #ans 
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+
+        # move along the input array starting from the end
+        for i in range(n):
+            idx = n - 1 - i
+            # set all the nines at the end of array to zeros
+            if digits[idx] == 9:
+                digits[idx] = 0
+            # here we have the rightmost not-nine
+            else:
+                # increase this rightmost not-nine by 1
+                digits[idx] += 1
+                # and the job is done
+                return digits
+
+        # we're here because all the digits are nines
+        return [1] + digits
 
 
 
@@ -28,6 +47,50 @@ class Solution(object):
 				S.insert(index, targets[index])
 				del S[index+len(targets[index]) : index+len(targets[index])+len(sources[index])]
 
+#ans
+class Solution(object):
+    def findReplaceString(self, S, indexes, sources, targets):
+        S = list(S)
+        for i, x, y in sorted(zip(indexes, sources, targets), reverse = True):
+            if all(i+k < len(S) and S[i+k] == x[k] for k in xrange(len(x))):
+                S[i:i+len(x)] = list(y)  #単純にyでも同様。しかし、[y]とすると一つの要素として認識されるが、結果は同じ。
+
+        return "".join(S)
+
+'''
+文字列はスライスで指定することはできるが、置き換えることができない。→一回リストに変換する必要あり。
+スライスして結合する方法でも可能。
+'''
+
+'''
+java solution
+class Solution {
+    public String findReplaceString(String S, int[] indexes, String[] sources, String[] targets) {
+        int N = S.length();
+        int[] match = new int[N];
+        Arrays.fill(match, -1);
+
+        for (int i = 0; i < indexes.length; ++i) {
+            int ix = indexes[i];
+            if (S.substring(ix, ix + sources[i].length()).equals(sources[i]))
+                match[ix] = i;
+        }
+
+        StringBuilder ans = new StringBuilder();
+        int ix = 0;
+        while (ix < N) {
+            if (match[ix] >= 0) {
+                ans.append(targets[match[ix]]);
+                ix += sources[match[ix]].length();
+            } else {
+                ans.append(S.charAt(ix++));
+            }
+        }
+        return ans.toString();
+    }
+}
+
+'''
 
 
 	def merge(self, intervals):
@@ -47,6 +110,10 @@ class Solution(object):
                 else:
         		ans.append(intervals[i])
 		return ans
+
+#ans
+
+
 
 
 
--- lcode/go/others/plus_one_linkedList.py
+++ lcode/go/others/plus_one_linkedList.py
@@ -11,32 +11,72 @@ class ListNode:
          self.val = val
          self.next = next
 
+    def __str__(self):
+        while self.next:
+            print( '%s' % (self.val))
+            self = self.next
+        return '%s' % (self.val)
+
+
 class Solution:
     def plusOne(self, head: ListNode) -> ListNode:
         sentinel = ListNode(0)
-        sentinel.next = head
         not_nine = sentinel
+        sentinel.next = head
+        
+        nine = None
 
-        while not_nine.next != 9:
-            not_nine = not_nine.next
-            if not not_nine.next:
-                break
-            nine = not_nine.next
+        while head:
+            if head.val != 9:
+                not_nine = head
+            head = head.next
 
         if not_nine == sentinel:
             not_nine.val = 1
         else:
             not_nine.val += 1
 
-
+        if not_nine.next:
+            nine = not_nine.next
+            
         while nine:
             nine.val = 0
             nine = nine.next
 
+        return sentinel if sentinel.val else sentinel.next
+    
+'''
+answer
+
+class Solution:
+    def plusOne(self, head: ListNode) -> ListNode:
+        # sentinel head
+        sentinel = ListNode(0)
+        sentinel.next = head
+        not_nine = sentinel
+
+        # find the rightmost not-nine digit
+        while head:
+            if head.val != 9:
+                not_nine = head
+            head = head.next
+
+        # increase this rightmost not-nine digit by 1
+        not_nine.val += 1
+        not_nine = not_nine.next
+
+        # set all the following nines to zeros
+        while not_nine:
+            not_nine.val = 0
+            not_nine = not_nine.next
+
+        return sentinel if sentinel.val else sentinel.next
+'''
 
 
-
-        return sentinel if sentinel.val else head
-
+if __name__ == '__main__':
+    s = Solution()
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(7, ListNode(9)))))
+    print(s.plusOne(head))
 
 
