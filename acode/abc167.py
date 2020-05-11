1ad80b8fdb2b601c3f53148a3140092e72222937
tinaba96 <tinaba178.96@gmail.com>
2020-05-11 09:28:45 +0900

    update

--- acode/abc167.py
+++ acode/abc167.py
@@ -23,6 +23,31 @@ else:
         val -= B
         print(ans-val)
 
+
+#ans
+#coding :utf-8
+
+A,B,C,K=map(int,input().split())
+
+result=0
+t=min(A,K)
+result+=t
+K-=t
+t=min(B,K)
+K-=t
+t=min(C,K)
+result-=t
+print(result)
+
+
+
+#ans
+# B - Easy Linear Programming
+
+a, b, c, k = map(int, input().split())
+print(min(k, a, a + a + b - k))
+
+
 #C
 from collections import defaultdict
 N, M, X = list(map(int, input().split()))
@@ -103,6 +128,28 @@ for i in range(len(li)-1, -1, -1):
         continue
 
 print(ans)
+#ans
+N, M, X = map(int, input().split())
+CA = [list(map(int, input().split())) for _ in range(N)]
+
+ans = 10 ** 18
+for i in range(2 ** N):
+    cost = 0
+    each_alg = [0 for _ in range(M)]
+    for j in range(N):
+        if (i >> j) & 1:
+            cost += CA[j][0]
+            for k in range(M):
+                each_alg[k] += CA[j][k+1]
+    if min(each_alg) >= X:
+        ans = min(ans, cost)
+
+if ans == 10**18:
+    print('-1')
+else:
+    print(ans)
+
+
 '''
 
 #D
