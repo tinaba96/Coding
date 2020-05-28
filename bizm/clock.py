fd1f829e15a494fd3f515022bac553712651ace9
tinaba96 <tinaba178.96@gmail.com>
2020-05-28 12:36:28 +0900

    update

--- bizm/clock.py
+++ bizm/clock.py
@@ -1,18 +1,16 @@
 class Solution:
     def clock_angle(hour, min):
-        ang_h = hour/6*180 + ang_m/180*15
-        ang_m = min/30*180
-       
+        ang_h = hour/6*180 + ang_m/180*15
+        ang_m = min/30*180
+        diff = abs(ang_h-ang_m)
+        if diff > 180:
+            ans = diff%180
+        else:
+            ans = diff
 
-diff = abs(ang_h-ang_m)
-if diff &gt; 180:
-    ans = diff%180
-else:
-    ans = diff
+        print(ans)
 
-print(diff)
-
-if __main__ == __init__:
-    s = Solution
-    s.clock_angle(13, 35)
+if __name__ == '_main__':
+    s = Solution
+    s.clock_angle(13, 35)
 
