4470e0eeb133b8d357e59ea15c7f74bc62b89223
tinaba96 <tinaba178.96@gmail.com>
2020-06-08 21:53:26 +0900

    update

--- bizm/sim.py
+++ bizm/sim.py
@@ -1,21 +1,85 @@
-nb_sim = 50
-M = 100
-N = 10000
+'''
+#1
+
+import random
+
+nb_sim = 50 #シミュレーション回数
+M = 10 #人数
+N = 1000000 #ループ処理回数
+money_each = 100000
 sim = []
-all = [100000 for i in range(M)]
+
+
 for j in range(nb_sim):
-cnt = 0
-for i in range(N):
-give = random
-get = random
-if all[give] &gt;= 130:
-all[give] -= 130
-all[get] += 130
+    all = [money_each for i in range(M)]
+    cnt = 0
+    for i in range(N):
+        give = random.randint(0, M-1)
+        while True:
+            get = random.randint(0, M-1)
+            if get != give:
+                break
 
-for money in all:
-if money == 0:
-cnt += 1
+        if all[give] >= 130:
+            all[give] -= 130
+            all[get] += 130
 
-sim.append(cnt)
+    #print(all)
+    for money in all:
+        if money == 0:
+            #print('a')
+            cnt += 1
 
-print(sum(sim)/50)
+    print(cnt)
+    sim.append(cnt)
+
+print(sum(sim)/nb_sim)
+
+'''
+
+
+class Simulation:
+    def __init__(self, nb_sim, M, N, money_each):
+        self.nb_sim = nb_sim
+        self.M = M
+        self.N = N
+        self.money_each = money_each
+
+
+
+    def simulation(self):
+        import random
+        sim = []
+        for j in range(self.nb_sim):
+            all = [self.money_each for i in range(self.M)]
+            cnt = 0
+            for i in range(self.N):
+                give = random.randint(0, self.M-1)
+                while True:
+                    get = random.randint(0, self.M-1)
+                    if get != give:
+                        break
+
+                if all[give] >= 130:
+                    all[give] -= 130
+                    all[get] += 130
+            #print(all)
+        
+            #print(all)
+            for money in all:
+                if money == 0:
+                    #print('a')
+                    cnt += 1
+
+            #print(cnt)
+            sim.append(cnt)
+
+        print(sim)
+
+        return sum(sim)/self.nb_sim
+
+if __name__ == '__main__':
+    s = Simulation(50, 5, 1000000, 1000)
+    print(s.simulation())
+
+
