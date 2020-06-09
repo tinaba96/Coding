12e33761f74566ea040a273799e1fdb3baf98b9b
tinaba96 <tinaba178.96@gmail.com>
2020-06-09 17:07:05 +0900

    update

--- lcode/go/medium/681.py
+++ lcode/go/medium/681.py
@@ -1,20 +1,26 @@
+
 class Solution:
     def nextClosestTime(self, time: str) -> str:
-        time.replace(":", "")
+        time = time.replace(':', '')
         elem = []
         for ele in time:
-            elem.append(int(ele))
+            elem.append(ele)
         elem.sort()
-        for ele in time:
-            if ele > time[4]:
-                return time[0]+time[1]+":"+time[3]+ele
-
-        if max(elem) < time[4]:
-            return time[0]+time[1]+":"+elem[1+elem.index(time[3])]+elem[0]
+        print(elem)
+        for ele in elem:
+            if ele > time[3]:
+                return time[0]+time[1]+":"+time[2]+ele
+        print(elem)
+        if max(elem) <= time[3]:
+            for ele in elem:
+                     if int(ele) <= 5 and ele > time[2]:
+                        return time[0]+time[1]+":"+ele+elem[0]
 
         h = int(time[0]+time[1])
         diff = 24
         flag = False
+        s = 0
+        t = 0
 
         for i in range(4):
             for j in range(4):
@@ -24,7 +30,7 @@ class Solution:
                         s = i
                         t = j
                 flag = True
-        
+
 
         if flag == False:
             return elem[0]+elem[0]+":"+elem[0]+elem[0]
@@ -33,18 +39,34 @@ class Solution:
 
 
 
+#ans
+class Solution(object):
+    def nextClosestTime(self, time):
+        cur = 60 * int(time[:2]) + int(time[3:])
+        allowed = {int(x) for x in time if x != ':'}
+        while True:
+            cur = (cur + 1) % (24 * 60)
+            if all(digit in allowed
+                    for block in divmod(cur, 60)
+                    for digit in divmod(block, 10)):
+                return "{:02d}:{:02d}".format(*divmod(cur, 60))
 
-                    
+#anotherans
+class Solution(object):
+    def nextClosestTime(self, time):
+        ans = start = 60 * int(time[:2]) + int(time[3:])
+        elapsed = 24 * 60
+        allowed = {int(x) for x in time if x != ':'}
+        for h1, h2, m1, m2 in itertools.product(allowed, repeat = 4):
+            hours, mins = 10 * h1 + h2, 10 * m1 + m2
+            if hours < 24 and mins < 60:
+                cur = hours * 60 + mins
+                cand_elapsed = (cur - start) % (24 * 60)
+                if 0 < cand_elapsed < elapsed:
+                    ans = cur
+                    elapsed = cand_elapsed
 
-
-
-
-
-        
-
-
-
-
+        return "{:02d}:{:02d}".format(*divmod(ans, 60))
 
 
 
