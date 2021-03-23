4d1901a572a4f684a53578c03113c3ae0556f686
tinaba96 <tinaba178.96@gmail.com>
2021-03-23 23:41:37 +0900

    first commit

--- acode/abc195/d.py
+++ acode/abc195/d.py
@@ -1,1 +1,12 @@
-N, M, Q = 
+N, M, Q = list(map(int, input().split()))
+things = []
+for n in range(N):
+    w, v = list(map(int, input().split()))
+    things.append((w, v))
+
+
+X = list(map(int, input().split()))
+
+for q in range(Q):
+
+
