bf0ee00e60465ce9608f3ed6d3586e81df8de141
tinaba96 <tinaba178.96@gmail.com>
2021-08-18 23:44:04 +0900

    first commit

---
+++ acode/typical90/001/main.py
@@ -0,0 +1,13 @@
+N, L = list(map(int, input().split()))
+K = int(input())
+K = list(map(int, input().split()))
+
+
+val = L / (k+1)
+
+for i in range(K+1):
+    le = 0
+    ri = 100000
+    mid = (le+ri)/2
+
+
