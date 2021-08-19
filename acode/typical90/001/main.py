145d3d8ccdb9c2c1c7d14860df9e334c25b42d6e
tinaba96 <tinaba178.96@gmail.com>
2021-08-19 21:26:00 +0900

    first commit

--- acode/typical90/001/main.py
+++ acode/typical90/001/main.py
@@ -1,13 +1,49 @@
 N, L = list(map(int, input().split()))
 K = int(input())
-K = list(map(int, input().split()))
+A = list(map(int, input().split()))
 
 
-val = L / (k+1)
 
-for i in range(K+1):
-    le = 0
-    ri = 100000
-    mid = (le+ri)/2
+left = 0
+right = 100000
+
+while left <= right:
+    #print('right', right)
+    #print('left', left)
+    mid = (left+right)//2
+    print('mid', mid)
+    tmp = 0
+    cnt = 0
+    minus = 0
+    #flag = False
+    for a in range(len(A)):
+        tmp = A[a] - minus
+        #print(A[a], minus)
+        if tmp >= mid:
+            minus = A[a]
+            cnt += 1
+            '''
+            if tmp == mid:
+                flag = True
+            '''
+        if cnt == K:
+            if mid < L-A[a]:
+                left = mid+1
+                '''
+                if flag:
+                    print(mid)
+                    exit()
+                '''
+            elif mid == L-A[a]:
+                print(mid)
+                exit()
+            else:
+                right = mid-1
+            break
+        elif a == len(A)-1 and cnt < K:
+            right = mid-1
+print(mid)
+            
+
 
 
