0bff8670b3b7fb87a69dcb21a6c498074c20cc27
tinaba96 <tinaba178.96@gmail.com>
2021-08-26 23:42:36 +0900

    first commit

--- acode/typical90/003/main.py
+++ acode/typical90/003/main.py
@@ -14,26 +14,29 @@ ans = 0
 dep = 0
 
 def dfs(used, start, dep, ans):
-    for i in dic[start]:
-        if used[i] == 0:
-            used[i] = 1
-            dep += 1
-            ans = max(ans, dep)
-            print('d',dep, '---', i)
-            ans = dfs(used, i, dep, ans)
-        else:
-            continue
+    for i in range(N+1):
+        if dic[start][i] == 1:
+            print(i,'->', used)
+            if used[i] == 0:
+                used[i] = 1
+                dep += 1
+                ans = max(ans, dep)
+                print('d',dep, '---', i)
+                ans = dfs(used, i, dep, ans)
+                break
+            else:
+                continue
     return ans
 
-#print('dic',dic[1])
+#print('dic',dic[2])
 
 for i in range(1,N+1):
     used = [0 for j in range(N+1)]
     #print(used)
     dep = 0
+    used[i] = 1
     ans = dfs(used, i, dep, ans)
     
-
 
 print(ans+1)
 
