1441a822fbba01d71e5ed8b5bb2c35de12c9ad69
tinaba96 <tinaba178.96@gmail.com>
2021-09-05 00:13:06 +0900

    first commit

--- acode/typical90/004/main.py
+++ acode/typical90/004/main.py
@@ -1,12 +1,20 @@
 H, W = list(map(int, input().split()))
 
 m = [[0 for i in range(W)] for j in range(H)]
+ans = [[0 for i in range(W)] for j in range(H)]
 
 
 for h in range(H):
     A = list(map(int, input().split()))
     for a in range(W):
         m[h][a] = A[a]
+
+#print(m)
+warr = [] 
+for k in range(W):
+    warr.append(sum(m[k]))
+
+print(warr)
 
 
 
