7debed5073b306820a8d3d9edd5c4332634e205c
tinaba96 <tinaba178.96@gmail.com>
2021-09-14 23:57:21 +0900

    first commit

--- acode/typical90/012/main.py
+++ acode/typical90/012/main.py
@@ -1,9 +1,19 @@
 H, W = list(map(int, input().split()))
 Q = int(input())
+ma = [[0 for i in range(W)] for j in range(H)]
+box = set()
 
 for i in range(Q):
     q = list(map(int, input().split()))
     if q[0] == 1:
+        ma[q[1]][q[2]] = 1
+    else:
+        if ma[q[1]][q[2]] == 1 and ma[q[3]][q[4]] == 1:
+            print('Yes')
+        else:
+            print('No')
+
+        
 
 
 
