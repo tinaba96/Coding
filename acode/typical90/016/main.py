656a1800e5b834564fa2039a47e2d13618b591ee
tinaba96 <tinaba178.96@gmail.com>
2021-09-22 00:05:24 +0900

    first commit

--- acode/typical90/016/main.py
+++ acode/typical90/016/main.py
@@ -1,6 +1,17 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-A.sort()
+ans = 0
+
+A.sort(reverse=True)
+
+for i in range(10000):
+    if i*A[0] < N:
+        continue
+    else:
+        
+
+
+#print(A)
 
 
