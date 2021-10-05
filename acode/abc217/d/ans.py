7ff85593f180a9522c3cd7815e15e8a96caf6bd3
tinaba96 <tinaba178.96@gmail.com>
2021-10-06 00:24:28 +0900

    first commit

---
+++ acode/abc217/d/ans.py
@@ -0,0 +1,11 @@
+import bisect
+import array
+l,q=map(int,input().split())
+a=array.array('i',[0,l])
+for _ in range(q):
+    c,x=map(int,input().split())
+    y = bisect.bisect(a,x)
+    if c==1:
+        a.insert(y,x)
+    else:
+        print(a[y]-a[y-1])
