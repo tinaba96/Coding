baf8ad4c41592d2eb1d2c0fa13a7b5ee0b43d3a8
tinaba96 <tinaba178.96@gmail.com>
2021-10-08 23:59:45 +0900

    first commit

---
+++ acode/abc217/d/main.py
@@ -0,0 +1,17 @@
+import bisect
+
+L, Q = list(map(int, input().split()))
+arr = [0, L]
+for q in range(Q):
+    c, x = list(map(int, input().split()))
+    if c == 1:
+        #bisect.insort_left(arr, x)
+        ind = bisect.bisect_left(arr, x)
+        arr.insert(ind, x)
+    else:
+        index = bisect.bisect_left(arr, x)
+        diff = arr[index]-arr[index-1]
+        print(diff)
+
+
+
