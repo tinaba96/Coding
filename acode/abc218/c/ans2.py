898923dc52c3555563ccae8a2e4dddfc8993d19c
tinaba96 <tinaba178.96@gmail.com>
2021-10-10 18:48:10 +0900

    first commit

---
+++ acode/abc218/c/ans2.py
@@ -0,0 +1,32 @@
+N = int(input())
+
+# グリッドを読み込んで#の位置の集合を返す
+def read():
+  S = set()
+  for y in range(N):
+    l = input()
+    for x in range(N):
+      if l[x]=="#":
+        S.add((x, y))
+  return S
+
+S = read()
+T = read()
+
+for _ in range(4):
+  # 最も左（複数あればそのうちで最も上）の#を(0, 0)の位置にする
+  mx, my = min(S)
+  S = set((x-mx, y-my) for x, y in S)
+  mx, my = min(T)
+  T = set((x-mx, y-my) for x, y in T)
+
+  if S==T:
+    print("Yes")
+    exit(0)
+
+  # Tを回転
+  T = set((y, -x) for x, y in T)
+
+print("No")
+
+
