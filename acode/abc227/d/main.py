640d42fdbf665a90016a9300ef0ece6431e5938f
tinaba96 <tinaba178.96@gmail.com>
2021-11-16 23:47:27 +0900

    first commit

---
+++ acode/abc227/d/main.py
@@ -0,0 +1,24 @@
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
+
+A = sorted(A, reverse=True)
+cnt = 0
+c = 0
+
+while (len(A)-c >= K):
+    m = min(A[:K])
+    if m <= 0:
+       print(cnt)
+       exit()
+    cnt += m
+    for k in range(K):
+        A[k] -= m
+        if A[k] <= 0:
+            c += 1
+    A = sorted(A, reverse=True)
+
+
+
+print(cnt)
+
+
