c9833b9c87a0f7ff23f528cab13817182b2e8f1e
tinaba96 <tinaba178.96@gmail.com>
2021-11-17 00:08:51 +0900

    first commit

--- acode/abc227/d/main.py
+++ acode/abc227/d/main.py
@@ -7,15 +7,18 @@ c = 0
 
 while (len(A)-c >= K):
     m = min(A[:K])
+    '''
     if m <= 0:
        print(cnt)
        exit()
+    '''
     cnt += m
-    for k in range(K):
+    for k in range(K): # O(NK)
         A[k] -= m
-        if A[k] <= 0:
+        if A[k] == 0:
             c += 1
-    A = sorted(A, reverse=True)
+    A = sorted(A, reverse=True) # O(n^2(logN))
+    #print(A)
 
 
 
