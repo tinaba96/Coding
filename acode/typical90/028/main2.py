3860a82d4f3963c84eae617798b584cdc7803158
tinaba96 <tinaba178.96@gmail.com>
2021-12-03 18:12:17 +0900

    first commit

--- acode/typical90/028/main2.py
+++ acode/typical90/028/main2.py
@@ -2,7 +2,7 @@ N = int(input())
 
 mp = [[0 for i in range(1000)] for j in range(1000)]
 
-ans[0 for i in range(N)]
+ans = [0 for i in range(N)]
 
 for i in range(N):
      lx, ly, rx, ry = list(map(int, input().split()))
@@ -13,12 +13,15 @@ for i in range(N):
 
 for y in range(1000):
     for x in range(1, 1000):
-        mp[x][y] += m[x-1][y] 
+        mp[x][y] += mp[x-1][y] 
 
 for x in range(1000):
     for y in range(1, 1000):
-        mp[x][y] += m[x][y-1] 
+        mp[x][y] += mp[x][y-1] 
         ans[mp[x][y]] += 1
 
+for i in range(N):
+    print(ans[i])
 
+print(ans)
 
