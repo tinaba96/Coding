c523a62a6c782e63776e162855f33a2e86f6f0e8
tinaba96 <tinaba178.96@gmail.com>
2021-12-07 00:00:56 +0900

    first commit

---
+++ acode/abc230/b/main.py
@@ -0,0 +1,22 @@
+N = str(input())
+
+if 'oxo' in N:
+    print('No')
+    exit()
+if 'ooo' in N:
+    print('No')
+    exit()
+if 'xxx' in N:
+    print('No')
+    exit()
+if 'xoo' in N:
+    print('No')
+    exit()
+if 'oo' in N:
+    print('No')
+    exit()
+
+
+print('Yes')
+
+
