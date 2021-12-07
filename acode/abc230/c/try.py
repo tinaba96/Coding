b055fc5ef84237ee627371ece98627509227b155
tinaba96 <tinaba178.96@gmail.com>
2021-12-08 00:43:36 +0900

    first commit

--- acode/abc230/c/try.py
+++ acode/abc230/c/try.py
@@ -1,12 +1,15 @@
-
 n,a,b = map(int,input().split())
 p,q,r,s = map(int,input().split())
 
-for i in range(p-1,q):
+#for i in range(p-1,q):
+for i in range(p,q+1):
     rowa  = ""
-    for j in range(r-1,s):
-        if abs(i+1-a) == abs(b-j-1) :
+    #for j in range(r-1,s):
+    for j in range(r,s+1):
+        #if abs(i+1-a) == abs(b-j-1) :
+        if (i-j)==(a-b) or (i+j)==(a+b):
             rowa += "#"
         else:
             rowa += "."
     print(rowa)
+
