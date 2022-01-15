a7ff103d0603586b02700ea680a1f32cf58473c0
tinaba96 <tinaba178.96@gmail.com>
2022-01-15 16:13:32 +0900

    update

--- acode/abc233/c/main.py
+++ acode/abc233/c/main.py
@@ -12,15 +12,17 @@ val = 1
 d = 0
 ans = []
 # 復習で追加
-ans_ans
+ans_ans = 0
 
-def cal(i, j, val, d, ans, ans_ans):
+def cal(i, j, val, d, ans):
+    # it should be global variables
+    global ans_ans
     val *= mp[i][j]
     if i+1 == N:
         if val == X:
             ans.append(1)
             ans_ans += 1
-        #print(val)
+            print(ans_ans)
         return
     for k in range(len(mp[i+1])):
         #print(ans)
@@ -31,7 +33,11 @@ for p in range(len(mp[0])):
     val = 1
     cal(0, p, val, d, ans)
 
-print(len(ans))
+#print(len(ans))
+print(ans_ans)
 #なぜansはintでは無理なのか
+# 変数の場合はglobal変数にしないといけない
+# 最定義を行った時点で、関数内で独立した変数として扱われるため
+# chttps://snowtree-injune.com/2018/07/29/post-738/
 
 
