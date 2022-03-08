195da3fa97377576d6b2a9afde5cc2f984627dc4
tinaba96 <tinaba178.96@gmail.com>
2022-03-09 00:17:10 +0900

    update

--- acode/abc242/c/main.py
+++ acode/abc242/c/main.py
@@ -9,6 +9,7 @@ cnt = 0
 mp = [[-1 for i in range(10)] for j in range(10000)]
 
 def memo(l, n, f):
+    # it is not good to have global variable since it becomaes hard to reuse
     global cnt
 
     if l == N-1:
@@ -21,7 +22,7 @@ def memo(l, n, f):
     if mp[l][n] != -1:
         if N-l > l:
             l += l
-            # (l is the integer u passed and not the rest)
+            #l is the integer u passed and not the rest
             cnt += mp[l][n]
         
     if n != 1 and n != 9:
@@ -41,6 +42,13 @@ for i in range(1, 10):
     memo(0, i, i)
 
 print(cnt)
+
+# this is not an approach like dp or memorization
+# for this approach, if you try to count part by part, you need to multiply not adding
+# and if you want find the memo that had maximum length, u need another O(N) to find it
+# but actually u cannot do part by part since you cannot know the first integer to start with. u have basically 3 differnt integer that has different memo length.
+# this leads to the solution shown in video tutorial
+# this approach does not use "漸化式", u should use the idea of "漸化式" in order to solve this efficiently
 
 
 
