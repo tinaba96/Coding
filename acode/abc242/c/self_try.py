e9a61b5fed03e10b1485ea21714ca695eef96e14
tinaba96 <tinaba178.96@gmail.com>
2022-03-10 01:36:05 +0900

    first commit

--- acode/abc242/c/self_try.py
+++ acode/abc242/c/self_try.py
@@ -1,34 +1,48 @@
 import sys
+import resource
 
-sys.setrecursionlimit(10**6)
+#print(resource.getrlimit(resource.RLIMIT_STACK))
+sys.setrecursionlimit(100000000)
 
 N = int(input())
 
 cnt = 0
 
-mp = [[-1 for i in range(10)] for j in range(10000000)]
+mp = [[-1 for i in range(10)] for j in range(N+1)]
 
 # 残りrで最後がdのとき、残りの埋め方
 def memo(r, d):
 
-    if n != 1 and n != 9:
+    if r == 1:
+        return 1
+    if mp[r][d] != -1:
+        return mp[r][d]
+
+    val = 0
+
+    if d != 1 and d != 9:
         val += memo(r-1, d-1)
         val += memo(r-1, d)
         val += memo(r-1, d+1)
-    elif n == 1:
+    elif d == 1:
         val += memo(r-1, d)
         val += memo(r-1, d+1)
-    elif n == 9:
+    elif d == 9:
         val += memo(r-1, d-1)
         val += memo(r-1, d)
+
+    mp[r][d] = val%998244353
+    return val%998244353
+
 
 ans = 0
 
 for i in range(1, 10):
-    memo(0, i, i)
+    #print(memo(N, i))
+    ans += memo(N, i)
 
 
-print(cnt)
+print(mp)
+print(ans%998244353)
 
-# i think it is possible to do it from left to right like what i tried in main.py
 
