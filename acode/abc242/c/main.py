f52236e60a26e75213480f0cce33281e9849ccb4
tinaba96 <tinaba178.96@gmail.com>
2022-03-09 23:58:51 +0900

    first commit

--- acode/abc242/c/main.py
+++ acode/abc242/c/main.py
@@ -44,11 +44,11 @@ for i in range(1, 10):
 print(cnt)
 
 # this is not an approach like dp or memorization
-# for this approach, if you try to count part by part, you need to multiply not adding
+# for this approach, if you try to count part by part, you need to multiply not adding -> although start integer are many, the length of each case should be same in order to do it part by part 
+# --->> multiply is not going to work. since 1 and 10 are not allowed, it cannot be simply done by multiplying (if you multiply, you can only get tyhe answer that can be got by multiplying and Prime number cannot be got bby multiplying) -> part by part is not going to work since those are not independent
 # and if you want find the memo that had maximum length, u need another O(N) to find it
 # but actually u cannot do part by part since you cannot know the first integer to start with. u have basically 3 differnt integer that has different memo length.
 # this leads to the solution shown in video tutorial
 # this approach does not use "漸化式", u should use the idea of "漸化式" in order to solve this efficiently
 
-
-
+# if you want to do it part by part and do multiplying, you need to know the last interger. You need to knoow the number of cases that ends with same integer (ex. 100 cases which end with 3) which is almost same as "総当たり"
