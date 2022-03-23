264368d29fea28ed4508f204cfffa7a86548c118
tinaba96 <tinaba178.96@gmail.com>
2022-03-24 00:18:47 +0900

    update

--- acode/dp/b/main.py
+++ acode/dp/b/main.py
@@ -1,14 +1,21 @@
+# this is same as 全探索i
+
+import sys
+sys.setrecursionlimit(10 ** 9)
+
 N, K  = list(map(int, input().split()))
 h  = list(map(int, input().split()))
 
-# K飛んで位置pに辿り着いたコスト
+# sから飛んで位置pに辿り着いたコスト
 def f(p):
     if p == 0:
         return 0
 
     mi = 10**5
     for i in range(1, K+1):
-        mi = min(mi, f(p-i))
+        if p-i >= 0:
+           mi = min(mi, abs(h[p]-h[p-i])+f(p-i))
+        # mi = min(mi, abs(h[i]-h[p-i]))
 
     return mi
 
