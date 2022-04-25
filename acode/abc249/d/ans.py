a30e9da78a14d436f782e9ab63d55ac4d84b7b08
tinaba96 <tinaba178.96@gmail.com>
2022-04-26 00:11:00 +0900

    update

--- acode/abc249/d/ans.py
+++ acode/abc249/d/ans.py
@@ -6,7 +6,7 @@ A = list(map(int, input().split()))
 A.sort()
 counter = Counter(A)
 
-ans = sum(counter[i] * counter[i // a] for a in A for i in range(a, A[-1] + 1, a))
+ans = sum(counter[i] * counter[i // a] for a in A for i in range(a, A[-1] + 1, a)) # aが基準　i//aがターゲット（積）
 print(ans)
 
 
