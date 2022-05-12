5bc46a0045686a33cfb0dcb36d3d47a89efb7320
tinaba96 <tinaba178.96@gmail.com>
2022-05-13 01:06:43 +0900

    update

---
+++ acode/abc246/d/ans2.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+ans=10**30
+j=10**6
+for i in range(10**6+1):
+    while (i+j)*(i**2+j**2)>=N and j>=0:
+        ans=min(ans,(i+j)*(i**2+j**2))
+        j-=1
+    '''
+    if ans==N or j<0:
+        break
+    '''
+
+print(min(ans,10**(18)))
