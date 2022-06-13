19e3f16d230f2f484b48ccb62f5fe6969524f60a
tinaba96 <tinaba178.96@gmail.com>
2022-06-14 00:42:35 +0900

    update

--- acode/abc254/c/main.py
+++ acode/abc254/c/main.py
@@ -38,4 +38,13 @@ print('Yes')
 # i think this approach is logically correct.
 # the only "No" answers is when the two numbers cannot swap.
 # if there is a case that cannot swap due the the way we keep some values as set() but answer should be "Yes", those numbers should be same numbers that are not needed to be swapped
+# if there are same number of vales or  more than K, it means it will not be "no" becuase of the set() (considered as one even if it has multiple
+'''
+K: 3
+A: 5 1 5 5 8 5
+sorted: 1 5 5 5 5 8 (-> "5" of index 4 can be realized anytime even if mp[] only has 1 because all the key in mp has at least one "5" and one of them must have 2 of "5")
+-> moreover, there no case that only one key has multiple "5". if some key has multiple, it means all have at leasr one.
+'''
+# wait above comment is not important
 
+#therfore, there is no problem that we use set() instead of array.
