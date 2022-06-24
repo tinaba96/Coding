b1eb78e984d855098de49f7fb98458ae5e7106bd
tinaba96 <tinaba178.96@gmail.com>
2022-06-25 00:36:16 +0900

    first commit

---
+++ acode/abc255/e/ans.py
@@ -0,0 +1,13 @@
+from collections import *
+f=lambda:map(int,input().split())
+n,m=f()
+s=[0,*f()]
+x=[*f()]
+for i in range(n-1):s[i+1]-=s[i]
+d=Counter()
+e=1
+for i in s:
+  for j in x:d[e*(j-i)]+=1
+  e*=-1
+print(max(d.values()))
+
