3c6a9db35160c06eb3487b8a5e3410b8b3fb8d9b
tinaba96 <tinaba178.96@gmail.com>
2022-07-17 18:43:54 +0900

    update

--- acode/typical90/033/main.py
+++ acode/typical90/033/main.py
@@ -2,8 +2,20 @@ H, W = list(map(int, input().split()))
 
 mp = [['.' for i in range(W) ] for j in range(H)]
 
+mp[0][0] = '#'
 
-print(mp)
+for w in range(W):
+    for h in range(H):
+        if mp[h][w] == '#':
+            mp[h+1][w] = '.'
+            mp[h][w+1] = '.'
+            mp[h+1][w+1] = '.'
+        else:
+
+
+
+
+#print(mp)
 
 
 
