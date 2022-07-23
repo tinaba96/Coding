d0966ba90b3ee272809df739e81ce42760d5bb7a
tinaba96 <tinaba178.96@gmail.com>
2022-07-23 22:35:58 +0900

    first commit

---
+++ acode/abc261/d/main.py
@@ -0,0 +1,13 @@
+N, M = list(map(int, input().split()))
+X = list(map(int, input().split()))
+
+bonus = [0 for g in range(N+1)]
+
+for i in range(M):
+    C, M = list(map(int, input().split()))
+    bonus[C] = M
+
+
+
+
+
