ec5e7f3bb8ae16f27f9f01cab78625e532fe4bba
tinaba96 <tinaba178.96@gmail.com>
2022-08-06 23:06:14 +0900

    first commit

---
+++ acode/abc263/c/main.py
@@ -0,0 +1,21 @@
+N, M = list(map(int, input().split()))
+
+import itertools
+
+l = []
+
+for i in range(M):
+    l.append(str(i+1))
+
+#if N == 1:
+
+
+
+m = []
+for v in itertools.combinations(l, N):
+    #print(sorted(list(v)))
+    #m.append(int(''.join(v)))
+    print(*v)
+ans = sorted(m)
+#print(ans)
+
