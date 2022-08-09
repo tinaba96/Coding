f2c7983c3c2f46f07f5c095e127fcb8aab2d4788
tinaba96 <tinaba178.96@gmail.com>
2022-08-10 01:15:38 +0900

    first commit

---
+++ acode/typical90/034/main.py
@@ -0,0 +1,27 @@
+N, K = list(map(int, input().split()))
+a = list(map(int, input().split()))
+
+mp = set()
+l = 0
+r = 0
+mp.add(a[l])
+
+while r < N::
+    r += 1 
+    if a[r] not in mp:
+        if len(mp) !== K:
+            mp.add(r)
+        else:
+            while l <= r:
+                l += 1
+                if a[l-1] != a[l]
+                    break
+
+
+
+
+
+
+
+
+
