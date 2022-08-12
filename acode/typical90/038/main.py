7b8c6ac03a8a22d784799195cb641a7869a9f0f0
tinaba96 <tinaba178.96@gmail.com>
2022-08-12 18:24:53 +0900

    update

--- acode/typical90/038/main.py
+++ acode/typical90/038/main.py
@@ -13,7 +13,32 @@ def fac(x):
             r[i] += 1
     return r
 
+"""nを素因数分解"""
+"""2以上の整数n => [[素因数, 指数], ...]の2次元リスト"""
 
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp!=1:
+        arr.append([temp, 1])
+
+    if arr==[]:
+        arr.append([n, 1])
+
+    return arr
+
+factorization(24) 
+
+## [[2, 3], [3, 1]] 
+##  24 = 2^3 * 3^1
         
 
 
