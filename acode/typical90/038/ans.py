c5e995ff4d038e2805cb3c66f111b5be978fbe66
tinaba96 <tinaba178.96@gmail.com>
2022-08-19 20:53:18 +0900

    first commit

---
+++ acode/typical90/038/ans.py
@@ -0,0 +1,14 @@
+A, B = map(int, input().split())
+
+def gcd(a, b):
+    if a < b: a, b = b, a
+    while a % b:
+        a, b = b, a % b
+    return b
+
+l = A // gcd(A, B)
+if l > 10**18 // B:
+    print('Large')
+else:
+    print(l * B)
+
