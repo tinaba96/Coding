ba8e5a0e2c49586f1f9d6949236c20c77ff46cba
tinaba96 <tinaba178.96@gmail.com>
2022-08-24 23:51:18 +0900

    update

--- acode/dp/d/main2.py
+++ acode/dp/d/main2.py
@@ -1,24 +1,26 @@
 N, W = list(map(int, input().split()))
 
-d = {}
+d = []
 
 for i in range(N):
     w, v = list(map(int, input().split()))
-    d[w] = v
+    d.append((w, v))
 
 # f(i, W) iまで判断して重さWの最大価値
 '''
 表
 Column: 見た数
-Row: 重さ、もしくは0、1
+Row: 重さ
 Value: 今の最大価値
 '''
 
-mp = [[0,0] for i in range(N)]
+mp = [[0, d[0][1]] for i in range(N)]
 
 for j in range(N-1):
-    mp[j+1][0] = max(mp[j][0], mp[j][1]+d[])
-    mp[j+1][1] = max(mp[j][0], mp[j][1]+)
+
+    mp[j+1][0] = max(mp[j][0], mp[j][1])
+
+    mp[j+1][1] = max(mp[j][0], mp[j][1]+d[j])
 
 
 
