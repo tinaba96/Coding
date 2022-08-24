af3db8206dcf35022ba90cbf6b8793f934591d32
tinaba96 <tinaba178.96@gmail.com>
2022-08-25 00:23:35 +0900

    update

--- acode/dp/d/main2.py
+++ acode/dp/d/main2.py
@@ -14,9 +14,16 @@ Row: 重さ
 Value: 今の最大価値
 '''
 
-mp = [[0, d[0][1]] for i in range(N)]
+#mp = [[0, d[0][1]] for i in range(N)]
+mp = [[0 for n in range(N)] for w in range(W)]
 
+mp[d[0][0]][0] = d[0][1]
+
+# 配るDP
 for j in range(N-1):
+    for k in range(W):
+        mp[d
+
 
     mp[j+1][0] = max(mp[j][0], mp[j][1])
 
