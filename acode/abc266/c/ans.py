5f728fcc2d0086c17d246cf80f0013bc4f9424d0
tinaba96 <tinaba178.96@gmail.com>
2022-08-29 23:11:05 +0900

    first commit

---
+++ acode/abc266/c/ans.py
@@ -0,0 +1,19 @@
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = list(map(int, input().split()))
+d = list(map(int, input().split()))
+
+combs = [(a, b, c), (b, c, d), (c, d, a), (d, a, b)]
+
+def check(points):
+    va = (points[1][0] - points[0][0], points[1][-1] - points[0][-1])
+    vb = (points[-1][0] - points[1][0], points[-1][-1] - points[1][-1])
+    g = va[0] * vb[-1] - va[-1] * vb[0]
+    if g <= 0:
+        print("No")
+        exit()
+
+for comb in combs:
+    check(comb)
+
+print("Yes")
