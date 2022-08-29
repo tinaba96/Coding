50bdeab91f96a30023cd200caa40231745959bd6
tinaba96 <tinaba178.96@gmail.com>
2022-08-29 23:14:38 +0900

    update

--- acode/abc266/c/ans.py
+++ acode/abc266/c/ans.py
@@ -9,7 +9,7 @@ def check(points):
     va = (points[1][0] - points[0][0], points[1][-1] - points[0][-1])
     vb = (points[-1][0] - points[1][0], points[-1][-1] - points[1][-1])
     g = va[0] * vb[-1] - va[-1] * vb[0]
-    if g <= 0:
+    if g <= 0: # 符号付き面積->負の場合、内角は180ど以上
         print("No")
         exit()
 
