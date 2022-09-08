b5ad051f5771d00b90857ed714521f74db70524f
tinaba96 <tinaba178.96@gmail.com>
2022-09-09 00:27:44 +0900

    update

--- acode/abc267/d/try.py
+++ acode/abc267/d/try.py
@@ -10,11 +10,13 @@ dp = [[-INF for i in range(M+1)] for j in range(N+1)]
 dp[0][0] = 0
 
 for x in range(N):
-    for t in range(M):
-        dp[x+1][t+1] = max(dp[x+1][t+1], dp[x][t]+((t+1)*A[x]))
+    for t in range(M+1):
+        if (t != M):
+            dp[x+1][t+1] = max(dp[x+1][t+1], dp[x][t]+((t+1)*A[x]))
         dp[x+1][t] = max(dp[x+1][t], dp[x][t])
 
-print(dp)
+#for i in range(N+1):
+#    print(dp[i])
 
 ans = 0
 for a in range(N+1):
