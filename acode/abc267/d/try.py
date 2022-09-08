3bd28db893373e3938eaccd57e6da450dbc6dc95
tinaba96 <tinaba178.96@gmail.com>
2022-09-08 23:55:21 +0900

    first commit

---
+++ acode/abc267/d/try.py
@@ -0,0 +1,25 @@
+N, M = list(map(int, input().split()))
+A = list(map(int, input().split()))
+
+# dp[i][m] iまで決めた上で、選んだ数mの時の最大ち
+
+INF = 100100100100
+
+dp = [[-INF for i in range(M+1)] for j in range(N+1)]
+
+dp[0][0] = 0
+
+for x in range(N):
+    for t in range(M):
+        dp[x+1][t+1] = max(dp[x+1][t+1], dp[x][t]+((t+1)*A[x]))
+        dp[x+1][t] = max(dp[x+1][t], dp[x][t])
+
+print(dp)
+
+ans = 0
+for a in range(N+1):
+    ans = max(ans, dp[a][M])
+
+print(dp[N][M])
+
+
