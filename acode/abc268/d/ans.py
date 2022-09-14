455c489894b54edc49455edca441f04ec3034174
tinaba96 <tinaba178.96@gmail.com>
2022-09-14 23:59:55 +0900

    first commit

---
+++ acode/abc268/d/ans.py
@@ -0,0 +1,26 @@
+from itertools import permutations
+
+def solve(n, m, s, t, min_chars=3, max_chars=16):
+    T = set(t)
+    def satisfied(x):
+        return (min_chars <= len(x) <= max_chars) and (not x in T)
+    def dfs(v, i, num, x):
+        if i == n:
+            return [x]
+        res = []
+        for k in range(1, num+1):
+            y = x + ("_" * k) + v[i]
+            res += dfs(v, i+1, num-k, y)
+        #print(res)
+        return res
+    num = max_chars - len("".join(s))
+    for v in permutations(s):
+        for x in dfs(v, 1, num, v[0]):
+            if satisfied(x):
+                return x
+    return -1
+
+n, m = map(int, input().split())
+s = [input() for i in range(n)]
+t = [input() for i in range(m)]
+print(solve(n, m, s, t))
---
+++ acode/abc268/d/test.py
@@ -0,0 +1,16 @@
+#A = list(map(int, input().split()))
+
+ans = []
+arr = []
+
+arr += [3]
+arr += [20]
+arr += [230]
+
+ans += [33]
+ans += arr
+ans += [233]
+
+print(ans)
+
+
