5960470bef3fe05996394c198900be010f76e30c
tinaba96 <tinaba178.96@gmail.com>
2022-12-07 01:26:40 +0900

    first commit

---
+++ acode/abc279/b/main.py
@@ -0,0 +1,13 @@
+import sys
+sys.setrecursionlimit(500005)
+#import pypyjit # this is for solving slow issue for pypy when using recursion but python will not need this (test will fail but submit works)
+#pypyjit.set_param('max_unroll_recursion=-1')
+
+
+S = str(input())
+T = str(input())
+
+
+print('Yes' if T in S else 'No')
+
+
