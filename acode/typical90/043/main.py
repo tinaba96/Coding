327dc69c2643e619f99c87c71a46ec72239b2a9b
tinaba96 <tinaba178.96@gmail.com>
2023-01-07 01:30:23 +0900

    first commit

---
+++ acode/typical90/043/main.py
@@ -0,0 +1,11 @@
+import sys
+sys.setrecursionlimit(500005)
+#import pypyjit # this is for solving slow issue for pypy when using recursion but python will not need this (test will fail but submit works)
+#pypyjit.set_param('max_unroll_recursion=-1')
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
+
+
