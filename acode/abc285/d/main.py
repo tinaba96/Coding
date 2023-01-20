4ec83e8b252f899abc75736ab9a2ffc31831df8e
tinaba96 <tinaba178.96@gmail.com>
2023-01-20 23:13:32 +0900

    first commit

---
+++ acode/abc285/d/main.py
@@ -0,0 +1,102 @@
+import sys
+sys.setrecursionlimit(500005)
+#sys.setrecursionlimit(10**9)
+#import pypyjit # this is for solving slow issue for pypy when using recursion but python will not need this (test will fail but submit works)
+#pypyjit.set_param('max_unroll_recursion=-1')
+
+
+from collections import defaultdict
+
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+
+N = int(input())
+
+now = set()
+
+mp = defaultdict(str)
+
+#uf = UnionFind(N)
+
+
+for i in range(N):
+    S, T = list(map(str, input().split()))
+    mp[S] = T
+
+check = set()
+
+def move(cur):
+    if cur in check:
+        print('No')
+        exit()
+
+    if cur in ignore:
+        return
+    if cur in mp:
+        check.add(cur)
+        move(mp[cur])
+    else:
+        return
+
+ignore = set()
+for e in mp:
+    check = set()
+    move(e)
+    ignore.add(e)
+
+
+
+print('Yes')
+
+
+
+
+
+
