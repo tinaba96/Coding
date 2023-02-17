10d453e488f91b027d96d3e552fc1b5a839c9853
tinaba96 <tinaba178.96@gmail.com>
2023-02-18 00:16:40 +0900

    first commit

--- acode/abc289/a/beforeTry.py
+++ acode/abc289/a/beforeTry.py
@@ -11,13 +11,19 @@ from collections import defaultdict
 #d = defaultdict(int)
 
 
-S = int(input())
+S = int(input(), 2)
 l = len(str(S))
 
 #print(bin(S | 10**2))
-#print(bin(~S))
-ans = bin(S | 11111111111)
+#print(bin(2**7-1))
+#print(S)
+ans = bin(S ^ 2**11-1)
+
+
+
+
 #print(format(~x & 0b1111, '04b'))
-print(str(ans))
+#print(ans)
+print(ans[len(ans)-l:])
 
 # or: 1 | 1 = 1 -> it will not reverse 
