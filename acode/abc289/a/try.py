196377d5cd0645d9721215376ab8933797a34e06
tinaba96 <tinaba178.96@gmail.com>
2023-02-18 17:59:36 +0900

    first commit

---
+++ acode/abc289/a/try.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(500005)
+#sys.setrecursionlimit(10**9)
+#import pypyjit # this is for solving slow issue for pypy when using recursion but python will not need this (test will fail but submit works)
+#pypyjit.set_param('max_unroll_recursion=-1')
+
+from collections import Counter
+#mylist = ["apple","banana","apple","apple","orange"]
+#mycounter = Counter(mylist)
+from collections import defaultdict
+#d = defaultdict(int)
+
+o = str(input())
+
+S = int(o, 2)
+l = len(str(o))
+
+#print(bin(S | 10**2))
+#print(bin(2**7-1))
+#print(S)
+ans = bin(S ^ 2**11-1)
+
+#print(format(~x & 0b1111, '04b'))
+#print(ans)
+print(ans[len(ans)-l:])
+
+print(S)
+print(len(str(S)))
+
+# or: 1 | 1 = 1 -> it will not reverse 
