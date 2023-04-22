cb37a5beb07cbf47257d307321ee1a92d1d7164c
tinaba96 <tinaba178.96@gmail.com>
2023-04-22 22:42:35 +0900

    first commit

---
+++ acode/abc299/f/main.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit(500005)
+#sys.setrecursionlimit(10**9)
+#import pypyjit # this is for solving slow issue for pypy when using recursion but python will not need this (test will fail but submit works)
+#pypyjit.set_param('max_unroll_recursion=-1')
+
+from collections import Counter
+#mylist = ["apple","banana","apple","apple","orange"]
+#mycounter = Counter(mylist)
+from collections import defaultdict
+#d = defaultdict(int)
+
+
+S = str(input())
+liS = list(S)
+
+c = Counter(liS)
+
+print(c)
+
+# T is substring of S???
+
