6ece7c7aee243c22c640c9b0d40ee409f6121e68
tinaba96 <tinaba178.96@gmail.com>
2023-04-27 07:28:29 +0900

    update

---
+++ acode/abc298/b/ans.py
@@ -0,0 +1,16 @@
+def solve():
+  n=int(input())
+  A=[list(map(int, input().split())) for _ in range(n)]
+  B=[list(map(int, input().split())) for _ in range(n)]
+
+  for _ in range(4):
+    if all(A[i][j] <= B[i][j] for i in range(n) for j in range(n)):
+      return True
+    #print('A: ', A)
+    #print(*A)
+    A=[list(a)[::-1] for a in zip(*A)]
+  return False
+
+print("Yes" if solve() else "No")
+
+
