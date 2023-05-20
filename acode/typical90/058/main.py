73aea156e610d5d2e9be5bfa93cdeda3931277f8
tinaba96 <tinaba178.96@gmail.com>
2023-05-21 00:07:11 +0900

    update

--- acode/typical90/058/main.py
+++ acode/typical90/058/main.py
@@ -13,11 +13,68 @@ from collections import defaultdict
 
 N, K = list(map(int, input().split()))
 
+if N == 0:
+    print(0)
+    exit()
 
 
-memoList = 
+memoList = [(0, 0) for i in range(45)] # when %10**5, it only comes 0-45
 
-while True:
+def oneLoop(v):
+    i = 0
+    while True:
+        if  v == 0 or v > 10**5:
+            v %= 10**5
+            break
+        s = str(v)
+        tmp = 0
+        for i in range(len(s)):
+            tmp += int(s[i])
+        v += tmp
+        i += 1
+    return v, i
 
+
+# preprocess
+for p in range(45):
+    a, b = oneLoop(p)
+    memoList[p] = (a, b)
+
+#print(memoList)
+
+oK = K
+
+a, b = oneLoop(N)
+K -= b
+
+t = a
+
+while K > 0:
+    #print(K)
+    if t == 0:
+        print(0)
+        exit()
+    if K >= memoList[t][1]:
+        t = memoList[t][0]
+        K -= memoList[t][1]
+    else:
+        break
+
+if K < 0:
+    K = oK
+    #print('yrs')
+
+
+# t is the starting index
+while K > 0:
+    #print(K)
+    s = str(t)
+    tmp = 0
+    for i in range(len(s)):
+        tmp += int(s[i])
+    t += tmp
+    K -= 1
+
+print(t)
 
 
