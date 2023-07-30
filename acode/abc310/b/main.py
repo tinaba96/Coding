7af30a580ceab982fd07453f2ed66b258df482af
tinaba96 <tinaba178.96@gmail.com>
2023-07-31 01:49:50 +0900

    first commit

---
+++ acode/abc310/b/main.py
@@ -0,0 +1,41 @@
+import sys
+sys.setrecursionlimit(500005)
+#sys.setrecursionlimit(10**9)
+#import pypyjit # this is for solving slow issue for pypy when using recursion but python will not need this (test will fail but submit works)
+#pypyjit.set_param('max_unroll_recursion=-1')
+
+from collections import Counter
+#mylist = ["apple","banana","apple","apple","orange"]
+#mycounter = Counter(mylist)
+from collections import defaultdict
+#d = defaultdict(int)
+
+mp = []
+FF = []
+
+N, M = list(map(int, input().split()))
+
+for i in range(N):
+    T = list(map(int, input().split()))
+    #P, C, F = list(map(int, input().split())) なぜ無理？
+    P = T[0]
+    C = T[1]
+    F = T[2:]
+
+    mp.append((P, C))
+    F = set(F)
+    FF.append(F)
+
+ans = 'No'
+
+for p in range(N):
+    for q in range(N):
+        if mp[p][0] >= mp[q][0]:
+            if (mp[p][0] > mp[q][0] or len(FF[p]) < len(FF[q])) and FF[p] <= FF[q]:
+                ans = 'Yes'
+                print(ans)
+                exit()
+            else:
+                continue
+
+print(ans)
