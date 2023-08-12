ee508993fce327bbd0a48758f76e1224b66ff1a1
tinaba96 <tinaba178.96@gmail.com>
2023-08-13 01:19:03 +0900

    first commit

---
+++ acode/abc314/a/try.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+number = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+
+print(number)
+
+#number = round(number, N)
+formatted_number = "{:.{}f}".format(number, N)
+print(formatted_number)
+
+umber = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+formatted_number = format(number, ".100f")
+print(formatted_number)
+
+# 倍精度浮動小数点数だから？１５桁くらいしか精度が保証されていないため。
