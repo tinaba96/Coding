7f4e9a54312e84cd8a6574347a4785f55665b198
tinaba96 <tinaba178.96@gmail.com>
2023-08-17 01:27:39 +0900

    update

---
+++ acode/abc314/a/main.py
@@ -0,0 +1,18 @@
+import sys
+sys.setrecursionlimit(500005)
+#sys.setrecursionlimit(10**9)
+#import pypyjit # this is for solving slow issue for pypy when using recursion but python will not need this (test will fail but submit works)
+#pypyjit.set_param('max_unroll_recursion=-1')
+
+from collections import Counter
+#mylist = ["apple","banana","apple","apple","orange"]
+#mycounter = Counter(mylist)
+from collections import defaultdict
+#d = defaultdict(int)
+
+e = '1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'
+
+N = int(input())
+
+print('3.' + e[:N])
+
