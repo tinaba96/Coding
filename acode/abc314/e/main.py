09a482216fd8c1245a00e8ae436b07dd459dfb01
tinaba96 <tinaba178.96@gmail.com>
2023-08-24 23:49:35 +0900

    first commit

---
+++ acode/abc314/e/main.py
@@ -0,0 +1,41 @@
+import sys
+sys.setrecursionlimit(500005)
+#sys.setrecursionlimit(10**9)
+#import pypyjit # this is for solving slow issue for pypy when using recursion but python will not need this (test will fail but submit works)
+#pypyjit.set_param('max_unroll_recursion=-1')
+
+from collections import Counter
+#mylist = ["apple","banana","apple","apple","orange"]
+#mycounter = Counter(mylist)
+from collections import defaultdict
+#d = defaultdict(int)
+
+mp = []
+N, M = list(map(int, input().split()))
+
+for i in range(N):
+    E = list(map(int, input().split()))
+    mp.append(E)
+
+ans = 0
+
+def ro(m, v):
+    global ans
+    if m <= 0:
+        ans += v
+        return 0
+    for i in range(N):
+        c, p, *s = mp[i]
+        #print(s)
+        for j in range(len(s)):
+            tmp = c*(s[j]/p)
+            ro(m-s[j], tmp)
+
+
+ro(M, 0)
+
+print(ans)
+
+# dp かと思ったけど、解法がよくわからない
+# 上記の方法は再帰潜りすぎかと思われる。
+
