2208a306d6117446f814eb43102f8f0f0eeba593
tinaba96 <tinaba178.96@gmail.com>
2023-10-03 23:41:29 +0900

    first commit

---
+++ acode/abc319/d/update2.py
@@ -0,0 +1,52 @@
+import sys
+sys.setrecursionlimit(500005)
+#sys.setrecursionlimit(10**9)
+#import pypyjit # this is for solving slow issue for pypy when using recursion but python will not need this (test will fail but submit works)
+#pypyjit.set_param('max_unroll_recursion=-1')
+
+from collections import Counter
+#mylist = ["apple","banana","apple","apple","orange"]
+#mycounter = Counter(mylist)
+from collections import defaultdict
+#d = defaultdict(int)
+
+
+N, M = list(map(int, input().split()))
+L = list(map(int, input().split()))
+
+# dp
+#print(sum(L))
+mini = sum(L)//M
+#print(mini)
+
+#print(cumsum)
+import bisect
+
+def ch(ind):
+    cnt = 1
+    v = ind
+
+    for ele in L:
+        if ind < ele:
+            return False
+
+        if v-ele < 0:
+            v = ind # initialize
+            cnt += 1
+            if cnt > M:
+                return False
+        elif v-ele != 0:
+            v -= 1
+        v -= ele
+
+    return True
+
+while True: # binary search? (size of Li is 10^9 so we need binary seach)
+    if ch(mini):
+        print(mini)
+        exit()
+    else:
+        mini += 1
+    
+
+# O(N*logN)
