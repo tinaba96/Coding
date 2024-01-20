a5e81d1786de5d80d69a0deb1e267fcfc230804f
tinaba96 <tinaba178.96@gmail.com>
2024-01-21 01:09:03 +0900

    final

--- practice/linkedList.py
+++ practice/linkedList.py
@@ -1,8 +1,8 @@
 class Node:
-    def __init__(self):
-        self.prev = null
-        self.next = null
-        self.value = null
+    def __init__(self, value=''):
+        self.prev = None
+        self.next = None
+        self.value = value
 
 # ini
 N = Node()
@@ -11,20 +11,33 @@ N.next = N
 
 # insert v after p
 def insertNode(v, p): 
+    v.next = p.next
+    p.next.prev = v
     p.next = v
     v.prev = p
-    v.next = p.next
 
 
 # deleting v
 def deleteNode(v):
-    #
+    v.prev.next = v.next
+    v.next.prev = v.prev
+
+    
 
 # print linked list as an array
-def printLinkedList(N):
-    #
+def printLinkedList():
+    v = N.next
+    while v != N:
+        print(v.value)
+        v = v.next
+
+#print(N)
+one = Node('one')
+two = Node('two')
+insertNode(one, N)
+insertNode(two, one)
 
 
-
+printLinkedList()
 
 
