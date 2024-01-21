d23ad5c7637c192ddb518ac48b49f75067815b43
tinaba96 <tinaba178.96@gmail.com>
2024-01-22 00:58:39 +0900

    first commit

---
+++ lcode/general/medium/130.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+
