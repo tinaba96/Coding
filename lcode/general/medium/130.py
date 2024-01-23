ceaaaeabf38ec417322a61ec6c123b1797e6adfb
tinaba96 <tinaba178.96@gmail.com>
2024-01-23 23:30:05 +0900

    update

--- lcode/general/medium/130.py
+++ lcode/general/medium/130.py
@@ -5,5 +5,17 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        visit = [[False for _ in range(n)] for p in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if board[m][n] == 'o':
+                    # BFS or DFS
+
+
+board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+Solution().solve(board)
+
+print(board)
+
 
 
