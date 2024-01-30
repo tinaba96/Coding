50e88f237b02942c278a774f028f071a37dfb22a
tinaba96 <tinaba178.96@gmail.com>
2024-01-30 23:57:49 +0900

    first commit

--- lcode/general/medium/198.py
+++ lcode/general/medium/198.py
@@ -4,16 +4,18 @@ class Solution:
     # dp[i][0, 1] -> max amount of money until index i
     def rob(self, nums: List[int]) -> int:
         l = len(nums)
-        dp = [[0 for _ in range(l+1)] for k in range(2)]
+        dp = [[0 for _ in range(2)] for k in range(l+1)]
         for i in range(1, l+1):
             # choose
-            dp[i][1] = max(dp[i][1], nums[i]+dp[i-1][0])
+            dp[i][1] = max(dp[i][1], nums[i-1]+dp[i-1][0])
             # not choose
             dp[i][0] = max(dp[i][0], max(dp[i-1][0], dp[i-1][1]))
+        #print(dp)
+        return(max(dp[l][0], dp[l][1]))   
 
-            
-
-
+#ans = Solution().rob([1,2,3,1])
+ans = Solution().rob([2, 7, 9, 3, 1])
+print(ans)
 
 
 
