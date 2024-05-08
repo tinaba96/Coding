c3fd4ec280f94f22c48d006f974de7aa98e6e9fb
tinaba96 <tinaba178.96@gmail.com>
2024-05-08 02:10:45 -0400

    update

--- lcode/general/medium/130.py
+++ lcode/general/medium/130.py
@@ -1,12 +1,14 @@
 from typing import List
 
+# gloval variable
+visit = [[False for _ in range(n)] for p in range(m)]
+d = [(-1, -1),(-1,1),(1, -1),(1,1)]
+
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        visit = [[False for _ in range(n)] for p in range(m)]
-        d = [(-1, -1),(-1,1),(1, -1),(1,1)]
         for i in range(m):
             for j in range(n):
                 visit[i][j] = True
@@ -14,9 +16,14 @@ class Solution:
                     continue
                 if board[m][n] == 'o':
                     # BFS or DFS
-                    for l, r in d:
 
-    def check():
+    def check(src, cur):
+        for l, r in d:
+            if board[l][r] = "0":
+                     check(cur, (l,r))
+
+
+        
 
 
 board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
@@ -24,5 +31,5 @@ Solution().solve(board)
 
 print(board)
 
-
+# is it possible to use stack with while loop
 
