e923d4fa760292a2f0c7498c00cb96337a9fe4c4
tinaba96 <tinaba178.96@gmail.com>
2024-05-13 23:01:34 -0400

    update

--- lcode/general/easy/108.py
+++ lcode/general/easy/108.py
@@ -1,15 +1,38 @@
 # https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
 
 # Definition for a binary tree node.
- class TreeNode:
+class TreeNode:
      def __init__(self, val=0, left=None, right=None):
          self.val = val
          self.left = left
          self.right = right
 class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+    def sortedArrayToBST(self, nums: []):
+    #def sortedArrayToBST(self, nums: []) -> Optional[TreeNode]:
         l = len(nums)
-        return TreeNode()
+        if l == 1:
+            return TreeNode(nums[0])
+        m = l//2
+
+        if nums[:m] == []:
+            if m+1 < l:
+                return TreeNode(nums[m], None, self.sortedArrayToBST(nums[m+1:]))
+            else:
+                return TreeNode(nums[m], None, None)
+
+        if nums[m:] == []:
+            return TreeNode(nums[m], self.sortedArrayToBST(nums[:m]), None)
+
+        if m+1 < l:
+            return TreeNode(nums[m], self.sortedArrayToBST(nums[:m]), self.sortedArrayToBST(nums[m+1:]))
+        else:
+            return TreeNode(nums[m], self.sortedArrayToBST(nums[:m]), None)
         
+
+p = [1,2]
+print(p)
+print(p[:1])
+print(p[1:])
+
 
 
