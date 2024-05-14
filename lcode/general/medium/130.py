52470785c0e0eee4d7c50e22520c8055a4b7575f
tinaba96 <tinaba178.96@gmail.com>
2024-05-14 01:17:38 -0400

    update

--- lcode/general/medium/130.py
+++ lcode/general/medium/130.py
@@ -11,16 +11,30 @@ class Solution:
         """
         for i in range(m):
             for j in range(n):
+                if (visit[i][j]):
+                    continue
                 visit[i][j] = True
                 if board[m][n] == 'x':
                     continue
                 if board[m][n] == 'o':
                     # BFS or DFS
+                    stack = []
+                    check(,[m,n])
 
     def check(src, cur):
         for l, r in d:
-            if board[l][r] = "0":
-                     check(cur, (l,r))
+            if cur[0]+l == src[0] and cur[1]+r == src[1]:
+                continue
+            if cur[0]+l < 0 or cur[1]+r < 0 or cur[0]+l < m or cur[1]+r < n:
+                stack = []
+                return
+            if board[cur[0]+l][cur[1]+r] == "O":
+                board[cur[0]+l][cur[1]+r] = True
+                stack.append((l,r))
+                check(cur, (l,r))
+        return
+
+
 
 
 board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
@@ -29,4 +43,5 @@ Solution().solve(board)
 print(board)
 
 # is it possible to use stack with while loop
+
 
