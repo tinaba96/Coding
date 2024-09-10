1010d2597e2a05cdaaaecdcb4338c261fc8842ac
tinaba96 <tinaba96.ca@gmail.com>
2024-09-11 01:40:34 +0900

    update

--- wday/oop.py
+++ wday/oop.py
@@ -1,2 +1,10 @@
 class student:
-    def __init__(self):
+    def __init__(self, id, name):
+        self.name = name
+        self.id = id
+
+class Course:
+    def __init__(self, course_id, course_name):
+        self.course = 
+
+
