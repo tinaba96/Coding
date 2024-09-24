7be623c6c89e3cacc8db7f872d378abaed658e00
tinaba96 <tinaba96.ca@gmail.com>
2024-09-24 17:37:09 +0900

    update

---
+++ wday/oop/mock.py
@@ -0,0 +1,21 @@
+
+class Student:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+class Course:
+    def __init__(self, title, descrip):
+        self.title = title
+        self.des = descrip
+
+
+Anna = new Student('Anna', 001)
+Kana = new Student('Kana', 002)
+
+
+Math = Course('Math', 'This is Math exam')
+Science = Course('Science', 'This is Science exam')
+
+
+
