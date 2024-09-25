09e65bf0df529857ca80361a32c46fc341bc4d3e
TakahiroInaba-bizmates <takahiro.inaba2@bizmates.co.jp>
2024-09-25 23:59:43 +0900

    first commit

--- wday/algo/string.py
+++ wday/algo/string.py
@@ -1,4 +1,34 @@
 # https://www.youtube.com/watch?v=Bbp_W3Jpk9I
 
+N = str(input())
+length = len(N)
+
+ans = 1
+tmpSet = set()
+start_p = 0
+end_p = -1
+
+for i in range(length):
+    v = N[i]
+    end_p = i
+    if v not in tmpSet:
+        tmpSet.add(v)
+    else:
+        for j in range(start_p, end_p):
+            start_char = N[j]
+            if start_char == v:
+                start_p = j+1
+                break
+            else:
+                tmpSet.remove(start_char)
+    ans = max(ans, end_p-start_p+1)
+
+print(ans)
+
+
+
+
+
+
 
 
