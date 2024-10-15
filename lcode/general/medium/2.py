d8fea3360de2dc60af693c2da60bdf5d9b72c080
tinaba96 <tinaba96.ca@gmail.com>
2024-10-16 00:14:28 +0900

    update

--- lcode/general/medium/2.py
+++ lcode/general/medium/2.py
@@ -5,6 +5,8 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        li
+        ans = ListNode()
+
+
 
         
